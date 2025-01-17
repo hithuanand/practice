5c74078d41b6a90e8562ef39c127783f2ed12d0b
hithu <hithuanand@gmail.com>
2025-01-17 21:11:54 +0530

    new if statement example

--- ifStatement2.py
+++ ifStatement2.py
@@ -1,6 +1,21 @@
-response = input("would you like food? (Y/N)")
+#response = input("would you like food? (Y/N)")
 
-if response == "Y":
-    print("Have some food!")
-else:
-    print("No food for you!")
+#if response == "Y":
+#    print("Have some food!")
+#else:
+#   print("No food for you!")
+
+
+#name = input("Enter your name")
+
+#if name == "":
+#    print("You did not type in your name")
+#else:
+#    print(f"Hello {name}")
+
+#for_sale = False
+
+#if for_sale:
+#    print("This item is for sale")
+#else:
+ #   print("This item is not for sale")
