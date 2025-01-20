4dff5a63cac63ee3bf013c4075028f2b434b6a8b
hithu <hithuanand@gmail.com>
2025-01-20 11:58:44 +0530

    new if statement example

---
+++ for_example.py
@@ -0,0 +1,26 @@
+
+#for x in reversed(range(1, 11)):
+#    print(x)
+#print("Happy New Year")
+
+#for x in range(1, 11, 3):
+#    print(x)
+
+#credit_card = "0123-4567-8901-2345"
+
+#for x in credit_card:
+#    print(x)
+
+
+#for x in range(1,21):
+#    if x ==13 :
+#        continue
+#    else:
+#        print(x)
+
+
+for x in range(1,21):
+    if x ==13 :
+        break
+    else:
+        print(x)
