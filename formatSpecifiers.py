4cd1ee653b6a054de0e0c5a1eebdf3ab4ef08659
hithu <hithuanand@gmail.com>
2025-01-20 11:25:25 +0530

    new if statement example

---
+++ formatSpecifiers.py
@@ -0,0 +1,21 @@
+# {value:flags}
+
+price1 = 30000.14159
+price2 = -98700.65
+price3 = 12000.23
+
+#print(f"Price 1 is ${price1:.3f}")
+#print(f"Price 2 is ${price2:.3f}")
+#print(f"Price 3 is ${price3:.3f}")
+
+#print(f"Price 1 is ${price1:<10}")
+#print(f"Price 2 is ${price2:<10}")
+#print(f"Price 3 is ${price3:<10}")
+
+#print(f"Price 1 is ${price1:>10}")
+#print(f"Price 2 is ${price2:>10}")
+#print(f"Price 3 is ${price3:>10}")
+
+print(f"Price 1 is ${price1:+,.2f}")
+print(f"Price 2 is ${price2:+,.2f}")
+print(f"Price 3 is ${price3:+,.2f}")
