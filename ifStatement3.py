98cdfa92df811b392618b5c75a6bab48157d9d3e
hithu <hithuanand@gmail.com>
2025-01-20 10:38:46 +0530

    new if statement example

---
+++ ifStatement3.py
@@ -0,0 +1,23 @@
+#conditional
+
+num = 5
+a = 6
+b = 7
+age = 18
+temperature = 23
+user_role = "guest"
+
+#result = "Even" if num % 2 == 0 else "Odd"
+#max_num = a if a > b else b
+#min_num = a if a < b else b
+
+#print(max_num)
+#print(min_num)
+
+#status = "Adult" if age >= 18 else "Child"
+#weather = "HOT" if temperature > 20 else "COLD"
+access_level = "Full Access" if user_role == "admin" else "Limited access"
+print(access_level)
+#print(weather)
+#print(status)
+#print(result)
