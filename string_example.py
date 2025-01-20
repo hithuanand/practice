d722ae848f01dd7057d3d211cb5a5c93a5f47e7b
hithu <hithuanand@gmail.com>
2025-01-20 11:01:44 +0530

    new if statement example

---
+++ string_example.py
@@ -0,0 +1,20 @@
+#variable string methods
+
+#name = input("Enter your full name: ")
+phone_number = input("Enter your phone number #:")
+#result = len(name)
+
+#result=name.find("A")
+
+#name = name.capitalize()
+#name = name.upper()
+#result = name.isdigit()
+#result = name.isalpha()
+#name = name.lower()
+
+#result = phone_number.count("-")
+result = phone_number.replace("-"," ")
+
+
+print(result)
+#print(name)
