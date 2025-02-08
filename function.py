81c27806b89b60ece530c933b8e6eff403032b34
hithu <hithuanand@gmail.com>
2025-02-08 12:11:58 +0530

    new if statement example

--- function.py
+++ function.py
@@ -7,8 +7,18 @@
 #
 # happy_birthday("Bro", 20)
 
-def display_invoice(username, amount, due_date):
-    print(f"Hello {username}")
-    print(f"your bill of ${amount:.2f} is due: {due_date}")
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"your bill of ${amount:.2f} is due: {due_date}")
+#
+# display_invoice("JoeSchmo", 100.01, "01/02")
 
-display_invoice("JoeSchmo", 100.01, "01/02")
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("spongebob", "squarepants")
+
+print(full_name)
