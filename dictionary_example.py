b548d6a77d5d97b1e47d3458810df00f37579dad
hithu <hithuanand@gmail.com>
2025-02-08 10:34:49 +0530

    new if statement example

---
+++ dictionary_example.py
@@ -0,0 +1,28 @@
+from orca.settings import capitalizationStyle
+
+capitals = {"USA": "Washinton DC",
+           "India": "New Delhi",
+           "China": "Beijing",
+           "Russia": "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+
+# if capitals.get("Russia"):
+#     print("That capital exits")
+# else:
+#     print("That capital doesnt exist")
+
+# capitals.update({"Germany":"Berlin"})
+# capitals.pop("china")
+
+keys = capitals.keys()
+
+print(keys)
+
+
+# keys = capitals.keys()
+# for key in capitals.keys(): #capitals.values()
+#     print(key)
+# print(capitals)
+
