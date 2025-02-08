e1a635308a75c11475e6874d2e6507bff5882987
hithu <hithuanand@gmail.com>
2025-02-08 11:16:51 +0530

    new if statement example

---
+++ game_rock_paper_scissors.py
@@ -0,0 +1,34 @@
+import random
+
+options = ("rock", "paper", "scissors")
+
+
+
+running = True
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors):")
+
+    print(f"player: {player}")
+    print(f"computer: {computer}")
+
+    if player == computer:
+        print (f"Its a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("you win!")
+    elif player == "paper" and computer == "rock":
+        print("you win!")
+    elif player == "scissors" and computer == "paper":
+        print("you win!")
+    else:
+        print("you lose")
+
+    play_again = input("play again? (y/n)").lower()
+
+    if not play_again == "y":
+        running = False
+print("Thanks for playing")
