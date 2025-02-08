90637822a7b06e10b19d3aa356c5a24e61872229
hithu <hithuanand@gmail.com>
2025-02-08 09:47:58 +0530

    new if statement example

---
+++ list_tuple_set.py
@@ -0,0 +1,46 @@
+# collection list [], tuple (), set {}
+
+# fruit = "apple"
+
+# fruits = ["orange", "apple", "banana", "coconut"]
+# print(fruits[3])
+
+# print(fruits[::-1])
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print(dir(fruits))
+
+# print(help(fruits))
+
+# print(len(fruits))
+
+# print("apple" in fruits)
+
+# fruits[0] = ("pinapple")
+
+# fruits.insert(0, "pineapple")
+
+# fruits.reverse()
+
+# print(fruits.index("apple"))
+# print(fruits.count("coconut"))
+# print(fruits)
+
+
+# fruits = {"apple", "banana", "orange", "coconut"}
+
+# fruits.add("pineapple") #remove, add, push, len
+
+# print(dir(fruits))
+# print(fruits)
+
+
+fruits =("apple", "orange", "pineapple", "banana")
+
+print(fruits)
+# print(dir(fruits))
+# print(help(fruits))
+
+
